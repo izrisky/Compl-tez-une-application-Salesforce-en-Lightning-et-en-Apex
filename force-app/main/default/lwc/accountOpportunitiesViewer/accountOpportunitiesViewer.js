f177075eaded5cfbd0155c90e6211a3c8887e5d8
izri <izri.brahim@softissime.com>
2024-12-23 10:58:31 +0100

    Exercice 1 & exercice 2

--- force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
+++ force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
@@ -1,26 +1,39 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getOpportunities from '@salesforce/apex/AccountOpportunitiesController.getOpportunities';
+import { refreshApex } from '@salesforce/apex';
 
 export default class AccountOpportunitiesViewer extends LightningElement {
     @api recordId;
     @track opportunities;
-    @track error = {};
+    @track wiredOpp;
+    @track error;
     columns = [
         { label: 'Nom Opportunité', fieldName: 'Name', type: 'text' },
         { label: 'Montant', fieldName: 'Amount', type: 'currency' },
         { label: 'Date de Clôture', fieldName: 'CloseDate', type: 'date' },
         { label: 'Phase', fieldName: 'StageName', type: 'text' }
     ];
-
-    @wire(getOpportunities, { recordId: '$accountId' }) //error
-    wiredOpportunities({ error, data }) {
-        if (data) {
-            this.opportunities = data;
-        } else if (error) {
+    @wire(getOpportunities, { accountId: '$recordId' }) 
+    wiredOpportunities(result) {
+        this.wiredOpp=result;
+        if (result.data) {
+            this.opportunities = result.data;
+            this.error = null;
+        } else if (result.error) {
+            this.opportunities = [];
             this.error = error;
-            this.opportunities = undefined;
+            console.error('Error loading opportunities:', error);
         }
     }
-
-
+    // Refresh logic for "Rafraîchir" button
+    async handleRafraichir() {    
+        try {            
+            await refreshApex(this.wiredOpp);
+            console.log(this.wiredOpp);
+        }
+        catch (e)
+        {
+            console.error('handleRafraichir loading opportunities:',e);
+        }
+    }
 }
